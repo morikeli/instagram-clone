62b3280608e142e1114b79126ff7b3efc44657af
bro_code <kelimbth@gmail.com>
2022-11-03 08:38:45 +0300

    created login, signup and logout views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib import messages
 
-# Create your views here.
+
+class UserLogin(LoginView):
+    template_name = 'accounts/login.html'
+
+def signup_view(request):
+
+    return render(request, 'accounts/signup.html')
+
+class LogoutUser(LogoutView):
+    template_name = 'accounts/logout.html'
