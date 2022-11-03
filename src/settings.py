210e823f91002b9c3e3bb85b912e05c6bd691909
bro_code <kelimbth@gmail.com>
2022-11-03 07:12:28 +0300

    added a slash

--- src/settings.py
+++ src/settings.py
@@ -123,8 +123,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/css'),
 ]
 
-MEDIA_URL = 'media'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediaFiles/')
+MEDIA_URL= 'MediaFiles'
+MEDIA_ROOT = os.path.join(BASE_DIR/'mediaFolder/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
