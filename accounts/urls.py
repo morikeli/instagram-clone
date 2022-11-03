731b718ce35d77acc5c17f9baddd764845c1147c
bro_code <kelimbth@gmail.com>
2022-11-03 09:14:16 +0300

    created url routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('login/', views.UserLogin.as_view(), name='user_login'),
+    path('create-account/', views.signup_view, name='signup'),
+    path('logged-out/', views.LogoutUser.as_view(), name='logout_user'),    
 ]
