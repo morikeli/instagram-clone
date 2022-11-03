a0507754e3a4ba7a8c365b23def7be0a9d059f3a
bro_code <kelimbth@gmail.com>
2022-11-03 08:24:20 +0300

    created a db table

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    id = models.CharField(max_length=15, primary_key=True, editable=False, unique=True)
+    name = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=7, blank=False)
+    country = models.CharField(max_length=30, blank=False)
+    phone_no = models.CharField(max_length=14, blank=False)
+    profile_pic = models.ImageField(upload_to='User-Dps/', default='default.png')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'Users Profile'
