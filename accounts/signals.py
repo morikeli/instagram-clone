1264b0d6936c4763cea601ef9abfb46ba9fc2988
bro_code <kelimbth@gmail.com>
2022-11-03 08:33:38 +0300

    added and configured model signals

---
+++ accounts/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import pre_save, post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import UserProfile
+import uuid
+
+@receiver(pre_save, sender=User)
+def generate_user_id(sender, instance, **kwargs):
+    if instance.id == "":
+        instance.id = str(uuid.uuid4()).replace('-', '')[:15]
+
+
+@receiver(post_save, sender=UserProfile)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(name=instance)
