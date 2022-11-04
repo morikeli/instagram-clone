da6ca9b436f9f5b1378fc750f274bdb11b47e967
bro_code <kelimbth@gmail.com>
2022-11-04 13:24:24 +0300

    created form for editing profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
+from .models import UserProfile
 
 class UserLoginForm(AuthenticationForm):
     def __init__(self, request=None, *args, **kwargs):
@@ -16,3 +16,12 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+class EditProfileForm(forms.ModelForm):
+    gender = forms.CharField(widget=forms.TextInput(attrs={'type': 'text', 'class': 'mb-2'}), disabled=True)
+    country = forms.CharField(widget=forms.TextInput(attrs={'type': 'text', 'class': 'mb-2'}), disabled=True)
+    phone_no = forms.CharField(widget=forms.TextInput(attrs={'type': 'tel', 'class': 'mb-2'}), disabled=True)
+
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
