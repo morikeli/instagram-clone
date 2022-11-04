5549d9b28b4f9e82f291a2c285d9275acd60dd98
bro_code <kelimbth@gmail.com>
2022-11-04 13:32:51 +0300

    added a url route

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('login/', views.UserLogin.as_view(), name='user_login'),
     path('create-account/', views.signup_view, name='signup'),
+    path('my-profile/', views.user_profile_view, name='user_profile'),
     path('logged-out/', views.LogoutUser.as_view(), name='logout_user'),    
 ]
