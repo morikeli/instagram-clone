e8356939c2111c647d02c541f286aeacc6093e2e
bro_code <kelimbth@gmail.com>
2022-11-07 09:47:22 +0300

    created and configured signals.py

---
+++ users/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver
+from .models import Posts
+import uuid
+
+@receiver(pre_save, sender=Posts)
+def generate_posts_id(sender, instance, **kwargs):
+    if instance.id == "":
+        instance.id = str(uuid.uuid4()).replace('-', '')[:15]
+
+@receiver(post_save, sender=Posts)
+def save_users_posts(sender, instance, created, **kwargs):
+    if created:
+        Posts.objects.create(user=instance)
