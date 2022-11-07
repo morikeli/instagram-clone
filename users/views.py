9392e471c64bbcaa745a5ff5e5f640fc2ba45a9f
bro_code <kelimbth@gmail.com>
2022-11-07 08:24:29 +0300

    created a homepage view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required(login_url='user_login')
+def homepage_view(request):
+
+    context = {}
+    return render(request, 'users/homepage.html', context)
