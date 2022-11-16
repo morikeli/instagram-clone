d2e4f1b12cc1c18cce291213e2413625805e0c1d
Mori Keli <kelimbth@gmail.com>
2022-11-16 05:36:24 +0300

    created a func. to generate liked posts id

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from accounts.models import UserProfile
-from .models import Posts, Comments, Friends
+from .models import Posts, Comments, Friends, LikedPost
 import uuid
 
 @receiver(pre_save, sender=Posts)
@@ -11,5 +11,10 @@ def generate_posts_id(sender, instance, **kwargs):
 
 @receiver(pre_save, sender=Friends)
 def generate_followers_id(sender, instance, **kwargs):
+    if instance.id == "":
+        instance.id = str(uuid.uuid4()).replace('-', '')[:15]
+
+@receiver(pre_save, sender=LikedPost)
+def generate_likedPosts_id(sender, instance, **kwargs):
     if instance.id == "":
         instance.id = str(uuid.uuid4()).replace('-', '')[:15]
