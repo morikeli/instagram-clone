e63172396c6b9e4622cae6ee80beefa869356bf6
brocode <kelimbth@gmail.com>
2023-03-17 18:45:17 +0300

    deleted create_user_profile signal

--- accounts/signals.py
+++ accounts/signals.py
@@ -9,9 +9,3 @@ def generate_user_id(sender, instance, **kwargs):
     if instance.id == "":
         instance.id = str(uuid.uuid4()).replace('-', '')[:20]
 
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        if instance.is_staff is False and instance.is_superuser is False:
-            User.objects.create(name=instance)
