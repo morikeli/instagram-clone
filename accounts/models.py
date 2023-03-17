87c36193ca4ea3532a016fe9bc147119dde349d5
brocode <kelimbth@gmail.com>
2023-03-17 16:22:52 +0300

    added new attributes and email table field

--- accounts/models.py
+++ accounts/models.py
@@ -5,26 +5,28 @@ from PIL import Image
 
 class User(AbstractUser):
     id = models.CharField(max_length=20, primary_key=True, editable=False, unique=True)
+    email = models.EmailField(unique=True, blank=False)
     bio = models.TextField()
     gender = models.CharField(max_length=7, blank=False)
     country = models.CharField(max_length=30, blank=False)
     phone_no = models.CharField(max_length=14, blank=False)
     profile_pic = models.ImageField(upload_to='User-Dps/', default='default.png')
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
     class Meta:
-        ordering = ['name']
+        ordering = ['username']
         verbose_name_plural = 'Profiles'
 
-        def __str__(self):
-            return f'{self.username}'
-
-        def save(self, *args, **kwargs):
-            super(User, self).save(*args, **kwargs)
-
-            dp = Image.open(self.profile_pic.path)
-            
-            if dp.height > 400 and dp.width > 400:
-                output_size = (400, 400)
-                dp.thumbnail(output_size)
-                dp.save(self.profile_pic.path)
-
+    def __str__(self):
+        return f'{self.username}'
+    
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+        dp = Image.open(self.profile_pic.path)
+        
+        if dp.height > 400 and dp.width > 400:
+            output_size = (400, 400)
+            dp.thumbnail(output_size)
+            dp.save(self.profile_pic.path)
