0e9128e6aa3ae1b0a6f6b74601856552dacc2f5b
brocode <kelimbth@gmail.com>
2023-03-20 12:29:33 +0300

    updated admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import User
+from .forms import SignUpForm
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(User)
+class UserLayout(UserAdmin):
+    model = User
+    add_form = SignUpForm
+    list_display = ['username', 'email', 'gender', 'date_joined', 'is_staff']
+
+admin.site.register(User, UserLayout)
