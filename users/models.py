3cf4ec8ffd524fb72f28634c46c57b593804cfab
brocode <kelimbth@gmail.com>
2023-03-20 14:37:36 +0300

    updated table field attr.

--- users/models.py
+++ users/models.py
@@ -56,7 +56,7 @@ class Comments(models.Model):
 
 class LikedPost(models.Model):
     id = models.CharField(max_length=15, primary_key=True, editable=False, unique=True)
-    post_id = models.CharField(max_length=15, blank=False, editable=False)
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
     liked = models.DateTimeField(auto_now_add=True)
 
