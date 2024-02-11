7517eb8a42d0f47bedabc0f030c12b45dfdd869f
brocode <kelimbth@gmail.com>
2024-02-11 21:26:17 +0300

    update signup path and add htmx paths

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 from . import views
+from . import validators
 
 urlpatterns = [
-    path('login/', views.UserLogin.as_view(), name='user_login'),
-    path('create-account/', views.signup_view, name='signup'),
+    path('login/', views.UserLogin.as_view(), name='login'),
+    path('create-account/', views.SignupView.as_view(), name='signup'),
     path('profile', views.profile_view, name='user_profile'),
     path('edit/', views.edit_profile_view, name='edit_profile'),
 
     path('logged-out/', views.LogoutUser.as_view(), name='logout_user'),    
 ]
+
+htmx_urlpatterns = [
+    path('validate-username/', validators.check_username_exists, name='validate_username'),
+    path('validate-email/', validators.email_address_validation, name='validate_email'),
+    path('validate-mobile/', validators.mobile_number_validation, name='validate_mobile_number'),
+    path('check-password/', validators.password_match_and_length_validation, name='check_password'),
+]
+
+urlpatterns += htmx_urlpatterns
