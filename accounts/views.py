8563d5eb4b242e06af0dbd437370df938e5b803c
brocode <kelimbth@gmail.com>
2024-02-11 21:25:31 +0300

    create class based SignupView

--- accounts/views.py
+++ accounts/views.py
@@ -1,29 +1,43 @@
+from django.utils.decorators import method_decorator
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib import messages
+from django.views import View
 from .forms import UserLoginForm, SignUpForm, EditProfileForm
 from .models import User
 from users.models import Posts, Friends
+
 
 class UserLogin(LoginView):
     authentication_form = UserLoginForm
     template_name = 'accounts/login.html'
 
-def signup_view(request):
-    form = SignUpForm()
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
+
+class SignupView(View):
+    form_class = SignUpForm
+    template_name = 'accounts/signup.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+
+        context = {'SignupForm': form}
+        return render(request, self.template_name, context)
+    
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
         if form.is_valid():
             form.save()
-            
+
             messages.success(request, 'Account created successfully!')
-            return redirect('user_profile')
+            return redirect('login')
 
-    context = {'SignUpForm': form}
-    return render(request, 'accounts/signup.html', context)
+        context = {'SignupForm': form}
+        return render(request, self.template_name, context)
 
-@login_required(login_url='user_login')
+
+@login_required(login_url='login')
 @user_passes_test(lambda user: user.is_staff is False)
 def edit_profile_view(request):
     form = EditProfileForm(instance=request.user)
