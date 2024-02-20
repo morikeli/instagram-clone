64e4ce47e48c6c188a26f1bdabc078e0f6e3d9c6
brocode <kelimbth@gmail.com>
2024-02-20 12:04:11 +0300

    delete '/' in templates dir. path

--- src/settings.py
+++ src/settings.py
@@ -50,7 +50,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR/'templates/',
+            BASE_DIR/'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
