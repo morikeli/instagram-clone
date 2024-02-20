8ebe6d16d2412e8cd341a0554a30281ed9f21198
brocode <kelimbth@gmail.com>
2024-02-20 16:37:51 +0300

    update signals

--- users/signals.py
+++ users/signals.py
@@ -7,19 +7,19 @@ import uuid
 @receiver(pre_save, sender=Post)
 def generate_posts_id(sender, instance, **kwargs):
     if instance.id == "":
-        instance.id = str(uuid.uuid4()).replace('-', '')[:15]
+        instance.id = str(uuid.uuid4()).replace('-', '')[:25]
 
 
 @receiver(pre_save, sender=Friend)
 def generate_followers_id(sender, instance, **kwargs):
     if instance.id == "":
-        instance.id = str(uuid.uuid4()).replace('-', '')[:15]
+        instance.id = str(uuid.uuid4()).replace('-', '')[:25]
 
 
 @receiver(pre_save, sender=LikedPost)
 def generate_likedPosts_id(sender, instance, **kwargs):
     if instance.id == "":
-        instance.id = str(uuid.uuid4()).replace('-', '')[:15]
+        instance.id = str(uuid.uuid4()).replace('-', '')[:25]
 
 
 @receiver(pre_save, sender=Tag)
@@ -28,18 +28,28 @@ def generate_tags_id(sender, instance, **kwargs):
         instance.id = str(uuid.uuid4()).replace('-', '')[:25]
 
 
-@receiver(post_save, sender=NewsFeed)
-def add_post(sender, instance, *args, **kwargs):
+@receiver(pre_save, sender=NewsFeed)
+def generate_news_feed_id(sender, instance, **kwargs):
+    if instance.id == "":
+        instance.id = str(uuid.uuid4()).replace('-', '')[:25]
+
+
+@receiver(post_save, sender=Post)
+def add_post(sender, instance, created, *args, **kwargs):
     """ This signal updates the current user's followers news feed when he/she posts something. """
     
-    post = instance
-    user = post.user
-    followers = Friend.objects.filter(follower=user)    # filter users following the current user
+    if created:
+        post = instance
+        user = post.user
+        followers = Friend.objects.filter(following=user)    # filter users following the current user
 
-    for follower in followers:
-        user_feed = NewsFeed(
-            post=post,
-            user=follower.follower,
-            date=post.date_posted,
-            following=user,   
-        )
+
+        for follower in followers:
+            user_feed = NewsFeed.objects.get_or_create(
+                post=post,
+                user=follower.follower,
+                date_posted=post.date_posted,
+                following=user,   
+            )
+        
+        return user_feed
