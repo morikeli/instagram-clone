4c02e23db6cdc24198dd82a9c380fd560d90e29d
brocode <kelimbth@gmail.com>
2024-02-20 11:35:53 +0300

    update CreatePostsForm

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,26 @@
 from django import forms
 from .models import Post, Comment
+from .utils import is_valid_media_file
+
 
 class CreatePostsForm(forms.ModelForm):
-    caption = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Type caption ...'}))
+    """ This form allows a user to post photos or videos on his/her news feed. """
+
+    caption = forms.CharField(widget=forms.Textarea(attrs={
+            'placeholder': 'Type caption ...'
+        }),
+        required=False,
+    )
+
+    image = forms.FileField(
+        widget=forms.FileInput(attrs={
+            'type': 'file', 'class': 'form-control mb-2', 'accept': '.3gpp, .jpg, .jpeg, .mp4, .mpeg, .ogg, .opus, .png, .wav',
+        }),
+        required=False,
+        validators=[is_valid_media_file],   # perform form validations
+    )
+
+
     class Meta:
         model = Post
-        fields = ['image', 'caption']
+        fields = ['caption', 'image']
