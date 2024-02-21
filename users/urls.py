726be9831c4063c60a876ebb689039f622616089
brocode <kelimbth@gmail.com>
2024-02-21 22:54:38 +0300

    updated urlpatterns and created htmx urlpatterns

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from . import views
+from . import htmx
+
 
 urlpatterns = [
-    path('homepage/', views.homepage_view, name='homepage'),
-    path('profile/<str:suggested_user>/', views.suggested_user_profile_view, name='follow_user_profile'),
-    path('like-post/', views.like_posts_view, name='like_post'),
-    path('delete-post/', views.delete_posts_view, name='delete_post'),
+    path('homepage/', views.HomepageView.as_view(), name='homepage'),
+    path('profile/<str:user_id>/', views.SuggestedUserProfileView.as_view(), name='view_user_profile'),
     
 ]
+
+htmx_urlpatterns = [
+    path('sugeested-user/follow/', htmx.follow_or_unfollow_users_homepage, name='follow_suggested_user'),
+    path('<str:user_id>/follow/', htmx.follow_or_unfollow_users_in_profile_page, name='follow_user'),
+    path('like/', htmx.like_or_unlike_post, name='like_or_unlike'),
+
+]
+
+urlpatterns += htmx_urlpatterns
