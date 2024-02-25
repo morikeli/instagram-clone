fc8b20daf1335dde3ef7aec1248a6b8f299e7e1e
brocode <kelimbth@gmail.com>
2024-02-25 17:22:16 +0300

    update: deleted notifications_qs

--- users/contrib/context_processors.py
+++ users/contrib/context_processors.py
@@ -2,8 +2,7 @@ from ..models import Notification
 
 
 def notifications(request):
-    notification_qs = Notification.objects.filter(receiver=request.user)
-    total_notifications = notification_qs.count()
+    total_notifications = Notification.objects.filter(receiver=request.user).count()
 
-    context = {'notifications': notification_qs, 'total_notifications': total_notifications}
+    context = {'total_notifications': total_notifications}
     return context
