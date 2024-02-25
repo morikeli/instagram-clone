56d7dc0fa26bd653aa3c2c7c3573d8c06f29995e
brocode <kelimbth@gmail.com>
2024-02-25 17:40:24 +0300

    update: add is_read=False to count unread notifications

--- users/contrib/context_processors.py
+++ users/contrib/context_processors.py
@@ -2,7 +2,7 @@ from ..models import Notification
 
 
 def notifications(request):
-    total_notifications = Notification.objects.filter(receiver=request.user).count()
+    total_notifications = Notification.objects.filter(receiver=request.user, is_read=False).count()
 
     context = {'total_notifications': total_notifications}
     return context
