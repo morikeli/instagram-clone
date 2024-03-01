784dfb67e86edf06e7898ece51ac285170d33997
brocode <kelimbth@gmail.com>
2024-03-01 16:10:20 +0300

    changed DEBUG to False

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [env('WEB_DOMAIN')]
 
