f352cf8f62f48559c79b3e5619bf44a958dec261
brocode <kelimbth@gmail.com>
2024-03-01 15:35:22 +0300

    appended url path to serve media files

--- src/urls.py
+++ src/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('users/', include('users.urls')),
     path('messages/', include('dm.urls')),
     path('admin/', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
