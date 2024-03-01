2a255f7869206baace698461a0e83830ed254fe3
brocode <kelimbth@gmail.com>
2024-03-01 15:49:51 +0300

    appended whitenoise middleware

--- src/settings.py
+++ src/settings.py
@@ -43,6 +43,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'src.urls'
