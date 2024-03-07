dd0f2579a8fb488b68b665d0e44c58b3a1686d9a
brocode <kelimbth@gmail.com>
2024-03-07 16:31:37 +0300

    created ModelForm to post instagram stories

--- users/forms.py
+++ users/forms.py
@@ -28,3 +28,26 @@ class CreatePostsForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['caption', 'image']
+
+
+class PostInstagramStoryForm(forms.ModelForm):
+    """ This form is used to upload images in a user's instagram stories """
+
+    caption = forms.CharField(widget=forms.Textarea(attrs={
+            'placeholder': 'Type caption ...'
+        }),
+        required=False,
+    )
+
+    story = forms.FileField(widget=forms.FileInput(attrs={
+            'type': 'file',
+            'class': 'form-control mb-2',
+            'accept': '.3gpp, .jpg, .jpeg, .mp4, .mpeg, .ogg, .opus, .png, .wav',
+        }),
+        required=True,
+        validators=[is_valid_media_file],   # perform form validations
+    )
+
+    class Meta:
+        model = InstagramStory
+        fields = ['caption', 'story']
