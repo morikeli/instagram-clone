9ed774daf3b32b26b5af76b4d0f42ee35a515b42
magnus.prime <78599959+morikeli@users.noreply.github.com>
2025-10-16 12:36:25 +0300

    fix: set value for `DEBUG` as an env variable

--- src/settings.py
+++ src/settings.py
@@ -16,7 +16,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = [env('WEB_DOMAIN')]
 
