eefd1ec70c64575c09de0a6d49c4f8ec03743885
magnus.prime <78599959+morikeli@users.noreply.github.com>
2025-10-16 12:38:01 +0300

    feat: added endpoints for JWT token

--- src/urls.py
+++ src/urls.py
@@ -4,12 +4,23 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 urlpatterns = [
     path('auth/', include('accounts.urls')),
     path('users/', include('users.urls')),
     path('messages/', include('dm.urls')),
     path('admin/', admin.site.urls),
     re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+
+    # djangorestframework token endpoints
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
